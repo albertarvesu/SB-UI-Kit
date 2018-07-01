56451b6b4aafb93ea8c25b4259c1b65da22c55aa
albertarvesu <albertarvesu@gmail.com>
2018-07-01 17:46:44 +1000

    Few more fixes

--- src/Icon/Icon.stories.js
+++ src/Icon/Icon.stories.js
@@ -4,9 +4,9 @@ import { storiesOf } from '@storybook/react';
 import { Icon } from './';
 
 storiesOf('Icon', module)
-  .add('with settings', () => (
-    <Icon glyph="settings" size={64} className="icon" color="blue" />
+  .add('Cog', () => (
+    <Icon glyph="settings" color="blue" />
   ))
-  .add('with Twitter icon', () => (
-    <Icon glyph="twitter" size={100} className="icon" color="lightblue" />
-  ))
+  .add('Twitter', () => (
+    <Icon glyph="twitter" size={64} color="lightblue" />
+  ));
